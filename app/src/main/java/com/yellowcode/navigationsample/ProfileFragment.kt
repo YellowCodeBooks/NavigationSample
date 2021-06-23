ebcb3497e1b4632b06ddbb8c2c02d02edb9b017b
Yellow Code Books <yellowcode.books@gmail.com>
2021-06-23 17:50:11 +0700

    Source Code Phan 3

--- app/src/main/java/com/yellowcode/navigationsample/ProfileFragment.kt
+++ app/src/main/java/com/yellowcode/navigationsample/ProfileFragment.kt
@@ -5,13 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.navArgs
 
 class ProfileFragment : Fragment() {
+
+    val args: ProfileFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val name = args.nameArg
+        view.findViewById<TextView>(R.id.tvWelcome)?.text = getString(R.string.welcome, "$name")
     }
 }
